6b854b1ede8a5ec5620c0774b55b6203a63499f6
pony <yiliny0214@gmail.com>
2019-01-05 15:26:43 +0800

    marked the pybind11 include directory

--- superfastcode2/setup.py
+++ superfastcode2/setup.py
@@ -7,7 +7,7 @@ cpp_args = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7']
 
 sfc_module = Extension(
     'superfastcode2', sources=['module.cpp'],
-    include_dirs=['pybind11/include'],
+    include_dirs=['C:/Users/yilin/source/repos/pybind11/include'],  # pybind11 include directory
     language='c++',
     extra_compile_args=cpp_args,
     )
